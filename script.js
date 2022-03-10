a93f9ded12229136d869518d740b9bd71e9f3994
edo979 <prezentabilan@gmail.com>
2022-03-10 23:29:29 +0100

    open close modals on button click

--- script.js
+++ script.js
@@ -3,7 +3,10 @@ const navTogglerBtn = document.getElementById('nav-toggler'),
   openModalBtn = document.getElementById('open-modal'),
   modalEl = document.getElementById('modal'),
   modalSuccessEl = document.getElementById('modal-success'),
-  closeModalBtn = document.getElementById('modal-close')
+  closeModalBtn = document.getElementById('modal-close'),
+  radioBtns = document.querySelectorAll('input[name=pledge]'),
+  modalCardEls = document.querySelectorAll('.modal-card'),
+  finishBtn = document.getElementById('finish-btn')
 
 // Event listeners
 
@@ -17,3 +20,25 @@ openModalBtn.addEventListener('click', (e) => {
 })
 
 closeModalBtn.addEventListener('click', (e) => (modalEl.style.display = 'none'))
+
+modalEl.addEventListener('click', (e) => {
+  // Set active clase
+  if (e.target.getAttribute('name') === 'pledge') {
+    const modalCardEl = e.target.parentElement.parentElement.parentElement
+
+    modalCardEls.forEach((card) => card.classList.remove('modal-card--active'))
+
+    modalCardEl.classList.add('modal-card--active')
+  }
+
+  // Activate success-modal
+  if (e.target.classList.contains('footer-form__button')) {
+    modalEl.style.display = 'none'
+    modalSuccessEl.style.display = 'grid'
+  }
+})
+
+finishBtn.addEventListener(
+  'click',
+  (e) => (modalSuccessEl.style.display = 'none')
+)
