44bff6dd9f82cf940b98f16724c1a8ee2b4cf042
edo979 <prezentabilan@gmail.com>
2022-03-10 21:13:40 +0100

    show hide modal on button click

--- script.js
+++ script.js
@@ -1,6 +1,19 @@
 const navTogglerBtn = document.getElementById('nav-toggler'),
-  navEl = document.querySelector('.nav')
+  navEl = document.querySelector('.nav'),
+  openModalBtn = document.getElementById('open-modal'),
+  modalEl = document.getElementById('modal'),
+  modalSuccessEl = document.getElementById('modal-success'),
+  closeModalBtn = document.getElementById('modal-close')
+
+// Event listeners
 
 navTogglerBtn.addEventListener('click', (e) => {
   navEl.classList.toggle('nav--expanded')
 })
+
+openModalBtn.addEventListener('click', (e) => {
+  e.preventDefault()
+  modalEl.style.display = 'grid'
+})
+
+closeModalBtn.addEventListener('click', (e) => (modalEl.style.display = 'none'))
